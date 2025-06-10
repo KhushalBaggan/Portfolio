752d65ddf5e87921a5b9ebff029ebe8f6b5be89c
Khushal Baggan <khushalbaggan506@gmail.com>
2025-06-10 12:43:51 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
 
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ["Front Developer","Web Developer","Full Stack Developer","Mern Stack Developer"], 
+      strings: ["Front Developer"], 
       startDelay: 100,
       typeSpeed: 50,
       backSpeed: 10,
@@ -30,8 +30,8 @@ const Home = () => {
     <h2 className="text-4xl sm:text--7xl font-bold text-white">I'm a <span ref={el}></span></h2>
     <p className="text-gray-300 py-4 max-w-md">
     I'm <b>Khushal Baggan.</b>
-    Currently, I love to work on Full Stack application using technologies like
-    React, Tailwind, Bootstrap, Node.js, Express.js, MongoDb,etc...
+    Currently, I love to work on Frontend application using technologies like
+    React, Tailwind, Bootstrap,etc...
     </p>
 
     <div>
